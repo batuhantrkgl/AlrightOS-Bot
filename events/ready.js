16d71986f0372bbcea2a135c5f8f637769be46af
batuhantrkgl <batuhanturkoglu37@gmail.com>
2023-04-21 21:58:30 +0300

    CONSOLE LOG SUPREMACY

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,14 @@ module.exports = {
 	once: true,
 	execute(client) {
 
-		console.log(`Discord API' connected to ${client.user.tag}...`);
+		console.log(`================================================================`)
+		console.log(`> Discord API' connected to ${client.user.tag}...`);
+		console.log(`> Coded by @Batuhantrkgl, with helps of AlrightTeam.`)
+		console.log(`> GitHub: https://github.com/batuhantrkgl`)
+		console.log(`> Repo: https://github.com/batuhantrkgl/AlrightOS-Bot`)
+		console.log(`> Telegram: https://t.me/AlrightOS`)
+		console.log(`> Telegram (Personal, may not reply.) https://t.me/batuhantrkgl`)
+		console.log(`================================================================`)
+
 	},
 };
