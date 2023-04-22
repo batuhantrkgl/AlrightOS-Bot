551d2518278f1d51d356b46e15cdc837b2ef7747
batuhantrkgl <batuhanturkoglu37@gmail.com>
2023-04-22 14:40:04 +0300

    MORE CONSOLE LOG SUPREMACY

--- events/ready.js
+++ events/ready.js
@@ -1,16 +1,30 @@
+const fs = require('node:fs')
+
 module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-
+		fs.readdir('./commands', (err, files) => {
+  		if (err) {
+    		console.error(err);
+    		return;
+ 		 }
+  
+  		const jsFiles = files.filter(file => file.endsWith('.js'));
+  
+  		jsFiles.forEach(file => {
+    	console.log(` | ${file}`);
+  			});
+		}); 
 		console.log(`================================================================`)
-		console.log(`> Discord API' connected to ${client.user.tag}...`);
+		console.log(`> Discord API connected to ${client.user.tag}...`);
 		console.log(`> Coded by @Batuhantrkgl, with helps of AlrightTeam.`)
 		console.log(`> GitHub: https://github.com/batuhantrkgl`)
 		console.log(`> Repo: https://github.com/batuhantrkgl/AlrightOS-Bot`)
 		console.log(`> Telegram: https://t.me/AlrightOS`)
 		console.log(`> Telegram (Personal, may not reply.) https://t.me/batuhantrkgl`)
 		console.log(`================================================================`)
+		console.log(`--> Commands Found (on /commands/ directory):`)
 
 	},
 };
